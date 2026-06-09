dcb450ae5b8452f16d9399aa401b17c356cb548b
taminmash <tamin.mashoori@gmail.com>
2026-06-09 23:30:41 +0200

    Update database.py

--- database.py
+++ database.py
@@ -30,3 +30,19 @@ def add_comment(post_id, comment_text):
 
     conn.commit()
     conn.close()
+
+
+def get_comment_count(post_id):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT COUNT(*) FROM comments WHERE post_id = ?",
+        (post_id,)
+    )
+
+    count = cur.fetchone()[0]
+
+    conn.close()
+
+    return count
