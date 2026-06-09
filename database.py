be96c8f574f38411ad6e2def49a90396cffc8653
taminmash <tamin.mashoori@gmail.com>
2026-06-09 23:27:45 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,19 @@
-# database functions
+import sqlite3
+
+DB_NAME = "comments.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        post_id INTEGER,
+        comment_text TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
