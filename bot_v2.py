82d06b36426d223b717481977e191dc6822d2c2d
taminmash <tamin.mashoori@gmail.com>
2026-06-16 00:49:27 +0200

    Update bot_v2.py

--- bot_v2.py
+++ bot_v2.py
@@ -1,1 +1,24 @@
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
 
+from config_v2 import BOT_TOKEN
+from database.db import init_db
+from handlers.start import start
+
+
+def main():
+    init_db()
+
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Vitrin Spain V2 Started")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
