f405590336fd2ab64777d37d52aae7524561b362
taminmash <tamin.mashoori@gmail.com>
2026-06-16 01:53:20 +0200

    Update bot_v2.py

--- bot_v2.py
+++ bot_v2.py
@@ -1,19 +1,33 @@
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
+    filters,
 )
 
 from config_v2 import BOT_TOKEN
 from database.db import init_db
+
 from handlers.start import start
+from handlers.menu import menu_handler
 
 
 def main():
+
     init_db()
 
     app = Application.builder().token(BOT_TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        CommandHandler("start", start)
+    )
+
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            menu_handler
+        )
+    )
 
     print("Vitrin Spain V2 Started")
 
