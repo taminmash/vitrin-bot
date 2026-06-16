1f3ebb27bd3a45ca2da820ebf61cf76c405b4de6
taminmash <tamin.mashoori@gmail.com>
2026-06-16 02:36:14 +0200

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,46 +1,36 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from handlers.profile import profile_start
-
 
 async def menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    print("MENU:", update.message.text)
+    if not update.message:
+        return
 
     text = update.message.text
 
-    if text == "🟡 ثبت آگهی در ویترین":
+    print("MENU =", text)
 
-        await update.message.reply_text(
-            "دسته آگهی را انتخاب کنید:\n\n"
-            "💼 کار و درآمد\n"
-            "🏠 خانه و اجاره\n"
-            "🛒 خرید و فروش\n"
-            "🔧 خدمات\n"
-            "💰 سرمایه گذاری\n"
-            "💶 خرید و فروش یورو\n"
-            "📢 آگهی ویژه"
-        )
-
-    elif text == "🟣 ثبت پیام در حیاط خلوت":
-
-        await update.message.reply_text(
-            "نوع پیام را انتخاب کنید:\n\n"
-            "💬 پیام ناشناس\n"
-            "🧭 تجربه‌ها\n"
-            "🎉 دورهمی\n"
-            "📰 اخبار"
-        )
-
-    elif text == "👤 پروفایل من":
+    if text == "👤 پروفایل من":
 
         await update.message.reply_text(
             "تست پروفایل"
         )
 
+    elif text == "🟡 ثبت آگهی در ویترین":
+
+        await update.message.reply_text(
+            "تست ویترین"
+        )
+
+    elif text == "🟣 ثبت پیام در حیاط خلوت":
+
+        await update.message.reply_text(
+            "تست حیاط خلوت"
+        )
+
     elif text == "ℹ️ راهنما":
 
         await update.message.reply_text(
-            "راهنمای ویترین اسپانیا به زودی تکمیل می‌شود."
+            "تست راهنما"
         )
