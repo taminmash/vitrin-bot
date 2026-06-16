7db4156054dab2f8446b0dee5ee2ac2188410822
taminmash <tamin.mashoori@gmail.com>
2026-06-16 02:38:35 +0200

    Update bot_v2.py

--- bot_v2.py
+++ bot_v2.py
@@ -6,10 +6,12 @@ from telegram.ext import (
 )
 
 from config_v2 import BOT_TOKEN
+
 from database.db import init_db
 
 from handlers.start import start
 from handlers.menu import menu_handler
+from handlers.profile import profile_handler
 
 
 def main():
@@ -24,8 +26,15 @@ def main():
 
     app.add_handler(
         MessageHandler(
-            filters.ALL,
+            filters.TEXT & ~filters.COMMAND,
             menu_handler,
+        )
+    )
+
+    app.add_handler(
+        MessageHandler(
+            filters.TEXT & ~filters.COMMAND,
+            profile_handler,
         )
     )
 
