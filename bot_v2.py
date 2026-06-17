0e61ccef9ea8bf35108c42d1a58221f9ef72de84
taminmash <tamin.mashoori@gmail.com>
2026-06-17 11:49:54 +0200

    Update bot_v2.py

--- bot_v2.py
+++ bot_v2.py
@@ -6,13 +6,10 @@ from telegram.ext import (
 )
 
 from config_v2 import BOT_TOKEN
-
 from database.db import init_db
 
 from handlers.start import start
 from handlers.menu import menu_handler
-from handlers.profile import profile_handler
-from handlers.post_create import post_handler
 
 
 def main():
@@ -23,20 +20,6 @@ def main():
 
     app.add_handler(
         CommandHandler("start", start)
-    )
-
-    app.add_handler(
-        MessageHandler(
-            filters.TEXT & ~filters.COMMAND,
-            profile_handler,
-        )
-    )
-
-    app.add_handler(
-        MessageHandler(
-            filters.TEXT & ~filters.COMMAND,
-            post_handler,
-        )
     )
 
     app.add_handler(
