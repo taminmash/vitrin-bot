939ce718f6879551fcaa77490d5facd25fb1fabf
taminmash <tamin.mashoori@gmail.com>
2026-06-17 14:27:31 +0200

    Update post_create.py

--- handlers/post_create.py
+++ handlers/post_create.py
@@ -3,25 +3,23 @@ from telegram.ext import ContextTypes
 
 from handlers.admin import send_post_to_admin
 
+
 async def start_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-```
-context.user_data["post_step"] = "test"
+    context.user_data["post_step"] = "test"
 
-await update.message.reply_text(
-    "ثبت آگهی تستی"
-)
-```
+    await update.message.reply_text(
+        "ثبت آگهی تستی"
+    )
+
 
 async def post_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-```
-if "post_step" not in context.user_data:
-    return
+    if "post_step" not in context.user_data:
+        return
 
-await update.message.reply_text(
-    "post handler ok"
-)
+    await update.message.reply_text(
+        "post handler ok"
+    )
 
-context.user_data.clear()
-```
+    context.user_data.clear()
