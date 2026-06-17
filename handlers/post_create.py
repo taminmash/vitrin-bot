fa136c9fd74655fd70d4b0f738657580f961b97f
taminmash <tamin.mashoori@gmail.com>
2026-06-17 14:07:59 +0200

    Update post_create.py

--- handlers/post_create.py
+++ handlers/post_create.py
@@ -1,10 +1,27 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from handlers.admin import send_post_to_admin
 
 async def start_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("TEST")
 
+```
+context.user_data["post_step"] = "test"
+
+await update.message.reply_text(
+    "ثبت آگهی تستی"
+)
+```
 
 async def post_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+```
+if "post_step" not in context.user_data:
     return
+
+await update.message.reply_text(
+    "post handler ok"
+)
+
+context.user_data.clear()
+```
