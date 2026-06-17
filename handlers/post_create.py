46f88dbbf382fc38fc6cbcf3eb8cae06c07107c2
taminmash <tamin.mashoori@gmail.com>
2026-06-17 14:06:03 +0200

    Update post_create.py

--- handlers/post_create.py
+++ handlers/post_create.py
@@ -1,16 +1,10 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
-async def start_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-```
-await update.message.reply_text(
-    "ثبت آگهی موقتاً غیرفعال است."
-)
-```
+async def start_post(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("TEST")
+
 
 async def post_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-```
-return
-```
+    return
